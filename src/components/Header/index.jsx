f081c899fed6b29b77ed400223627499dcc69b26
VítorLF <vitorlimaferreira122@gmail.com>
2024-02-21 11:07:39 -0300

    Fazendo upload dos arquivos

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,15 @@
+import "./style.css";
+
+export function Header(props){
+
+    return (
+        <header>
+
+            <div className="container">
+                <h1>{props.title}</h1>
+                <h2>{props.subTitulo}</h2>
+            </div>
+            
+        </header>
+    )
+}
